47545017d0519b438c246c707352560c851ba445
jmartincampos <jmartincampos18@gmail.com>
2024-04-24 16:59:38 -0700

    changes package.json/vite.config.js/app.js

--- React Portfolio/my-react-portfolio/src/App.js
+++ React Portfolio/my-react-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Portfolio from '/Users/jonathancampos/Desktop/React-Portfolio/React Portfolio/my-react-portfolio';
 
 function App() {
   return (
